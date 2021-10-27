05bbbfe761b1cb9fc12c3469e297b441edd415c2
StartForKiller <jesussanz2003@gmail.com>
2021-10-27 09:15:09 +0200

    Prueba 3

---
+++ prueba3.py
@@ -0,0 +1,7 @@
+nombre = input("¿Cual es tu nombre?:")
+nota1 = float(input("Primera nota:"))
+nota2 = float(input("Segunda nota:"))
+
+notaMedia = (nota1 + nota2) / 2
+
+print("La nota media de {} es {}".format(nombre, notaMedia))
