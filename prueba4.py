006c1552965eee5509d345a03c362c951b2088f6
StartForKiller <jesussanz2003@gmail.com>
2021-10-27 09:44:49 +0200

    Práctica 2

---
+++ prueba4.py
@@ -0,0 +1,7 @@
+import math
+
+radio = float(input("¿Cuál es el radio?:"))
+
+area = math.pi * radio**2
+
+print("El área de un círculo de radio {} es {}".format(radio, area))
---
+++ prueba6.py
@@ -0,0 +1,9 @@
+metros = float("Dame la longitud en metros:")
+
+pulgadas = metros * 39.3701
+pies = metros * 3.28084
+yardas = metros * 1.094
+
+print("La logitud en pulgadas es {}".format(pulgadas))
+print("La logitud en pies es {}".format(pies))
+print("La logitud en yardas es {}".format(yardas))
